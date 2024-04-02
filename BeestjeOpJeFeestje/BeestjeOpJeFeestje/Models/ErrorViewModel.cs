5de600edd85da221b39c42484c26156d125ad177
Delluci <wmn.brunenberg@student.avans.nl>
2024-04-02 14:11:53 +0200

    Create project

---
+++ BeestjeOpJeFeestje/BeestjeOpJeFeestje/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace BeestjeOpJeFeestje.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
