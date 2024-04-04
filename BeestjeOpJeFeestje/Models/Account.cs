297f31249c1f173ad95a7df1e3791349eec9683d
Delluci <wmn.brunenberg@student.avans.nl>
2024-04-04 13:26:56 +0200

    fix(Account): add use statements and missing }

--- BeestjeOpJeFeestje/Models/Account.cs
+++ BeestjeOpJeFeestje/Models/Account.cs
@@ -2,9 +2,10 @@
 
 namespace Models {
     public class Account : IdentityUser {
-    public string Name { get; set; }
-    public int AddressId { get; set; }
-    public Address Address { get; set; }
-    public int CustomerCardId { get; set; }
-    public CustomerCard CustomerCard { get; set; }
+        public string Name { get; set; }
+        public int AddressId { get; set; }
+        public Address Address { get; set; }
+        public int CustomerCardId { get; set; }
+        public CustomerCard CustomerCard { get; set; }
+    }
 }
