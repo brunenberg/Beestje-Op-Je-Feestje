5257badfea555ce8df830de4079239f38c20f1a1
Joris Hageman <jorishageman04@gmail.com>
2024-04-04 12:28:55 +0200

    Update SeedData.cs

--- BeestjeOpJeFeestje/Models/SeedData.cs
+++ BeestjeOpJeFeestje/Models/SeedData.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Models;
+using Models.Data;
 
 public static class SeedData {
     public static async Task Initialize(IServiceProvider serviceProvider, UserManager<Account> userManager, RoleManager<IdentityRole> roleManager) {
