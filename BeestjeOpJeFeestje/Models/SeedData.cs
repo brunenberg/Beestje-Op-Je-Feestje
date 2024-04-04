460b9e65f340d422f440827a340282cb7a45efcf
Delluci <wmn.brunenberg@student.avans.nl>
2024-04-04 21:29:03 +0200

    feat(SeedData): implement better seeddata

--- BeestjeOpJeFeestje/Models/SeedData.cs
+++ BeestjeOpJeFeestje/Models/SeedData.cs
@@ -15,19 +15,18 @@ public static class SeedData {
             }
 
             // Create CustomerCards
-            var noCard = new CustomerCard { CardType = "Geen" };
-            var silverCard = new CustomerCard { CardType = "Silver" };
-            var goldCard = new CustomerCard { CardType = "Gold" };
+            var silverCard = new CustomerCard { CardType = "Zilver" };
+            var goldCard = new CustomerCard { CardType = "Goud" };
             var platinaCard = new CustomerCard { CardType = "Platina" };
 
-            context.CustomerCards.AddRange(noCard, silverCard, goldCard, platinaCard);
+            context.CustomerCards.AddRange(silverCard, goldCard, platinaCard);
             await context.SaveChangesAsync();
 
             // Create AnimalTypes
             var jungleType = new AnimalType { TypeName = "Jungle" };
-            var farmType = new AnimalType { TypeName = "Farm" };
-            var snowType = new AnimalType { TypeName = "Snow" };
-            var desertType = new AnimalType { TypeName = "Desert" };
+            var farmType = new AnimalType { TypeName = "Boerderij" };
+            var snowType = new AnimalType { TypeName = "Sneeuw" };
+            var desertType = new AnimalType { TypeName = "Woestijn" };
             var vipType = new AnimalType { TypeName = "VIP" };
 
             context.AnimalTypes.AddRange(jungleType, farmType, snowType, desertType, vipType);
@@ -57,12 +56,34 @@ public static class SeedData {
             await userManager.AddToRoleAsync(customerUser, "Customer");
             await userManager.AddToRoleAsync(adminUser, "Admin");
 
-            // Create Animals
-            for (int i = 1; i <= 30; i++) {
-                var animal = new Animal { Name = $"Animal {i}", Price = i, ImagePath = $"~/images/animal{i}.jpg", AnimalTypeId = i % 5 + 1 };
-                context.Animals.Add(animal);
-            }
 
+            // Type: Jungle - Aap, Olifant, Zebra, Leeuw
+            Animal aap = new Animal { Name = "Aap", Price = 20, ImagePath = "~/images/aap.jpg", AnimalTypeId = 1 };
+            Animal olifant = new Animal { Name = "Olifant", Price = 50, ImagePath = "~/images/olifant.jpg", AnimalTypeId = 1 };
+            Animal zebra = new Animal { Name = "Zebra", Price = 30, ImagePath = "~/images/zebra.jpg", AnimalTypeId = 1 };
+            Animal leeuw = new Animal { Name = "Leeuw", Price = 40, ImagePath = "~/images/leeuw.jpg", AnimalTypeId = 1 };
+
+            // Type: Boerderij - Hond, Ezel, Koe, Eend, Kuiken
+            Animal hond = new Animal { Name = "Hond", Price = 20, ImagePath = "~/images/hond.jpg", AnimalTypeId = 2 };
+            Animal ezel = new Animal { Name = "Ezel", Price = 30, ImagePath = "~/images/ezel.jpg", AnimalTypeId = 2 };
+            Animal koe = new Animal { Name = "Koe", Price = 30, ImagePath = "~/images/koe.jpg", AnimalTypeId = 2 };
+            Animal eend = new Animal { Name = "Eend", Price = 20, ImagePath = "~/images/eend.jpg", AnimalTypeId = 2 };
+            Animal kuiken = new Animal { Name = "Kuiken", Price = 40, ImagePath = "~/images/kuiken.jpg", AnimalTypeId = 2 };
+
+            // Type: Sneeuw - Pinguïn, IJsbeer, Zeehond
+            Animal pinguin = new Animal { Name = "Pinguïn", Price = 40, ImagePath = "~/images/pinguin.jpg", AnimalTypeId = 3 };
+            Animal ijsbeer = new Animal { Name = "IJsbeer", Price = 50, ImagePath = "~/images/ijsbeer.jpg", AnimalTypeId = 3 };
+            Animal zeehond = new Animal { Name = "Zeehond", Price = 30, ImagePath = "~/images/zeehond.jpg", AnimalTypeId = 3 };
+
+            // Type: Woestijn - Kameel, Slang
+            Animal kameel = new Animal { Name = "Kameel", Price = 40, ImagePath = "~/images/kameel.jpg", AnimalTypeId = 4 };
+            Animal slang = new Animal { Name = "Slang", Price = 20, ImagePath = "~/images/slang.jpg", AnimalTypeId = 4 };
+
+            // Type: VIP - T-Rex, Unicorn 
+            Animal trex = new Animal { Name = "T-Rex", Price = 100, ImagePath = "~/images/trex.jpg", AnimalTypeId = 5 };
+            Animal unicorn = new Animal { Name = "Unicorn", Price = 200, ImagePath = "~/images/unicorn.jpg", AnimalTypeId = 5 };
+
+            context.Animals.AddRange(aap, olifant, zebra, leeuw, hond, ezel, koe, eend, kuiken, pinguin, ijsbeer, zeehond, kameel, slang, trex, unicorn);
             await context.SaveChangesAsync();
         }
     }
