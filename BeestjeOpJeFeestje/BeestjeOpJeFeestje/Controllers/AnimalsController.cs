29aee4a4d64de7426220f5b65b376b0e76571860
Delluci <wmn.brunenberg@student.avans.nl>
2024-04-05 12:17:29 +0200

    feat(Authorization): add to animal crud and nav

--- BeestjeOpJeFeestje/BeestjeOpJeFeestje/Controllers/AnimalsController.cs
+++ BeestjeOpJeFeestje/BeestjeOpJeFeestje/Controllers/AnimalsController.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using Models;
 using Models;
 
 namespace BeestjeOpJeFeestje.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class AnimalsController : Controller
     {
         private readonly ApplicationDbContext _context;
