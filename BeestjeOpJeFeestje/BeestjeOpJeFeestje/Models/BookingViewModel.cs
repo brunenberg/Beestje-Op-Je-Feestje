5e12100672741a661875e80313e3859bb2e5397d
Joris Hageman <jorishageman04@gmail.com>
2024-04-07 17:11:51 +0200

    Wip: Validation

--- BeestjeOpJeFeestje/BeestjeOpJeFeestje/Models/BookingViewModel.cs
+++ BeestjeOpJeFeestje/BeestjeOpJeFeestje/Models/BookingViewModel.cs
@@ -1,21 +1,37 @@
 ﻿using Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
-namespace BeestjeOpJeFeestje.Models
-{
-    public class BookingViewModel
-    {
-
+namespace BeestjeOpJeFeestje.Models {
+    public class BookingViewModel {
         public List<Animal> Animals { get; set; }
-        public string SelectedDate { get; set; }
         public List<Animal>? SelectedAnimals { get; set; }
+
+        [Required(ErrorMessage = "Gelieve een datum te selecteren")]
+        public string SelectedDate { get; set; }
+
+        [Required(ErrorMessage = "Naam is verplicht")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "E-mailadres is verplicht")]
+        [EmailAddress(ErrorMessage = "Ongeldig e-mailadres")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Straatnaam is verplicht")]
         public string Street { get; set; }
+
+        [Required(ErrorMessage = "Huisnummer is verplicht")]
         public string HouseNumber { get; set; }
+
+        [Required(ErrorMessage = "Postcode is verplicht")]
+        [RegularExpression(@"^\d{4} ?[a-zA-Z]{2}$", ErrorMessage = "Ongeldige postcode")]
         public string PostalCode { get; set; }
+
+        [Required(ErrorMessage = "Stad is verplicht")]
         public string City { get; set; }
+
         public double TotalPrice { get; set; }
         public List<string> AppliedDiscounts { get; set; }
-        public Booking? Booking { get; set; }
+        public Booking Booking { get; set; }
     }
 }
